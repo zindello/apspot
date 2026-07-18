e2e10b7b2d05a87d55261785af2948d4b29b1ddd
Joshua Mesilane <josh@zindello.com.au>
2026-07-18 22:45:43 +1000

    Fixing APSPOT

--- lambdas/sns_sms.py
+++ lambdas/sns_sms.py
@@ -8,57 +8,73 @@ import urllib.parse
 from datetime import datetime
 
 APSPOTAPIURL = os.getenv('APSPOTAPIURL')
+messageLimit = 5
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
-def sendmessage(message, destination, awsNumber:
+def sendmessage(message, destination, awsNumber):
     snsclient = boto3.client('sns')
-    snsclient.publish(PhoneNumber=destination, Message=message, MessageAttributes={'AWS.MM.SMS.OriginationNumber': {'DataType': 'String', 'StringValue': awsNumber}})
+    #print('Sending Message: ' + message)
+    #print('To: ' + destination)
+    #print('From: ' + awsNumber)
+    snsclient.publish(PhoneNumber=destination, Message=message)
+    #snsclient.publish(PhoneNumber=destination, Message=message, MessageAttributes={'AWS.MM.SMS.OriginationNumber': {'DataType': 'String', 'StringValue': awsNumber}})
 
 def processmessage(action, message, activator=""):
-    logging.info("Detemine Message Type")
-    logging.info("Got a " + action + " message")
+    print("Detemine Message Type")
+    print("Got a " + action + " message")
     response = []
     if action == "spot":
-        logging.info("Preparing spot URL")
+        print("Preparing spot URL")
         url = APSPOTAPIURL + '/processmessage?action=spot&activator=' + activator + '&message=' + urllib.parse.quote_plus(message)
-        logging.info(url)
+        print(url)
     else:
-        logging.info("Preparing " + action + " URL")
+        print("Preparing " + action + " URL")
         url = APSPOTAPIURL + '/processmessage?action=' + action + '&message=' + urllib.parse.quote_plus(message)
-        logging.info(url)
-    apiresponse = requests.get(str(url))
-    logging.debug(apiresponse)
+        print(url)
+    apiresponse = requests.get(str(url), timeout=10)
+    #print(apiresponse)
     if apiresponse.status_code == 200:
-        logging.debug(apiresponse.text)
+        #print(apiresponse.text)
         for result in json.loads(apiresponse.text)['response']:
-            response.append(result)
+            response.append(str(result))
     else:
         response.append("ERROR CONNECTING TO APSPOT API")
     return response
 
 def lambda_handler(event, context):
     
-    print(event['Records'][0]['Sns']['Message'])
+    #print(event['Records'][0]['Sns']['Message'])
     message = json.loads(event['Records'][0]['Sns']['Message'])
     messagetext = message['messageBody'].upper()
     messagefrom = message['originationNumber']
     awsNumber = message['destinationNumber']
+    print('Message from: ' + messagefrom)
+    print('Incoming number: ' + awsNumber)
+    print('Message Subject: ' + messagetext)
+
+    if 'INREACHLINK' in messagetext:
+        messagetext = messagetext.split('INREACHLINK')[0] + ' [InReach]'
+    else:
+        messagetext = messagetext + ' [SMS]'
 
     if messagetext.startswith("!"):
-        response = processmessage("spot", messagetext.split()[1:], messagetext.split()[0][1:])
+        response = processmessage("spot", ' '.join(messagetext.split()[1:]), messagetext.split()[0][1:])
     elif messagetext.startswith('SPOTS'):
-        response = processmessage("spots", messagetext.split()[1:])
+        response = processmessage("spots", messagetext[6:])
+        if len(response) > messageLimit:
+            del response[messageLimit:]
     elif messagetext.startswith('?'):
-        response = processmessage("search", messagetext.split()[1:])
+        response = processmessage("search", messagetext[2:])
+        if len(response) > messageLimit:
+            del response[messageLimit:]
     else:
-        logging.info('Sending usage information')
-        response = sendusage("usage", messagetext.split()[1:])
+        print('Sending usage information')
+        response = processmessage("usage", messagetext)
 
     for reply in response:
+        print("Sending Message: " + reply + " to: " + messagefrom + " from: " + awsNumber)
         sendmessage(reply, messagefrom, awsNumber)
-
-
